da145a7ea86f74bbb955e277c61f3ba1836ed806
Victoria Ziegler <toriziegler@cox.net>
2022-06-18 10:33:36 -0700

    Feature 12 Complete

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from tasks.models import Task
 
 # Register your models here.
+
+
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Task, TaskAdmin)
