d5981499a3ad1d7d5faa10ecb57d378f123e7abc
Victoria Ziegler <toriziegler@cox.net>
2022-06-17 17:07:49 -0700

    Feature 3 Complete

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+USER_MODEL = settings.AUTH_USER_MODEL
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    # blank=True??? ^
+    members = models.ManyToManyField(USER_MODEL, related_name="projects")
+
+    def __str__(self):
+        return self.name
