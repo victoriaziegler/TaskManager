ef4dfee553898a3e55eb3ea0ac12efa505c8bf39
Victoria Ziegler <toriziegler@cox.net>
2022-06-21 17:03:30 -0700

    Feature 18 Complete

--- tracker/settings.py
+++ tracker/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "projects.apps.ProjectsConfig",
     "tasks.apps.TasksConfig",
+    "markdownify.apps.MarkdownifyConfig",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MARKDOWNIFY = {"default": {"BLEACH": False}}
